4050684249e7309a93067474a7d0075b45e1b2d8
Tufan Tunç <19643687+tufantunc@users.noreply.github.com>
2021-10-26 21:09:36 +0300

    test(unit tests): added missing unit test to cover 100%
    closes #2

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,6 +6,7 @@ import useIsMobile from '../index';
 let windowSize = 1024;
 
 const mockRemoveEventListener = jest.fn();
+const mockRemoveListener = jest.fn();
 let triggerEventListener;
 
 const getMatchMediaQueryResult = (query) => {
@@ -20,33 +21,79 @@ const getMatchMediaQueryResult = (query) => {
   return queryResult;
 }
 
-window.matchMedia = (query) => {
-  const queryResult = getMatchMediaQueryResult(query);
-
-  return {
-    media: query,
-    matches: queryResult,
-    addListener: (cb) => {
-      triggerEventListener = () => cb({matches: getMatchMediaQueryResult(query)});
-    },
-    removeListener: () => {},
-    addEventListener: (cb) => {
-      cb(queryResult);
-    },
-    removeEventListener: mockRemoveEventListener,
-  };
-};
-
-const DummyComp = ({mobileScreenSize = 768}) => {
-  const isMobile = useIsMobile(mobileScreenSize);
+const DummyComp = ({mobileScreenSize}) => {
+  const isMobile = useIsMobile(mobileScreenSize || undefined);
   return (
     <div>Screen is <span data-testid="test-data">{ isMobile ? 'mobile' : 'desktop'}</span></div>
   )
 };
 
+describe('useIsMobile hooks on unsupported browsers', () => {
+  it('shoud throw error if matchMedia is not supported', () => {
+    jest.spyOn(console, 'error');
+    console.error.mockImplementation(() => {});
+
+    expect(() => { render(<DummyComp />) }).toThrowError();
+
+    console.error.mockRestore();
+  });
+});
+
+describe('useIsMobile hooks on outdated browsers', () => {
+  beforeAll(() => {
+    jest.spyOn(React, 'useEffect').mockImplementation(React.useLayoutEffect);
+    Object.defineProperty(window, 'matchMedia', {
+      value: (query) => {
+        const queryResult = getMatchMediaQueryResult(query);
+      
+        return {
+          media: query,
+          matches: queryResult,
+          addListener: (cb) => {
+            cb({matches: getMatchMediaQueryResult(query)});
+          },
+          removeListener: mockRemoveListener,
+        };
+      },
+      writable: true
+    });
+  });
+
+  afterAll(() => {
+    React.useEffect.mockRestore();
+  });
+
+  it('it should works on outdated browsers', () => {
+    render(<DummyComp />);
+    const testData = screen.getByTestId('test-data');
+    expect(getNodeText(testData)).toBe('desktop');
+  });
+
+  it('it should call removeListener on unmount on outdated browsers', () => {
+    mockRemoveListener.mockRestore();
+    const {unmount} = render(<DummyComp />);
+    unmount();
+    expect(mockRemoveListener).toHaveBeenCalledTimes(1);
+  });
+});
+
 describe('useIsMobile hooks', () => {
   beforeAll(() => {
     jest.spyOn(React, 'useEffect').mockImplementation(React.useLayoutEffect);
+    Object.defineProperty(window, 'matchMedia', {
+      value: (query) => {
+        return {
+          media: query,
+          matches: getMatchMediaQueryResult(query),
+          addEventListener: (_name, cb) => {
+            triggerEventListener = () => cb({ matches: getMatchMediaQueryResult(query) });
+            triggerEventListener();
+          },
+          removeEventListener: mockRemoveEventListener,
+        };
+      },
+      writable: true
+    });
   });
 
   afterAll(() => {
@@ -97,4 +144,18 @@ describe('useIsMobile hooks', () => {
     unmount();
     expect(mockRemoveEventListener).toHaveBeenCalledTimes(1);
   });
+
+  it('should works as expected when gives custom mobile screen size', async () => {
+    windowSize = 768;
+    render(<DummyComp mobileScreenSize={320} />);
+    const testData = screen.getByTestId('test-data');
+    expect(getNodeText(testData)).toBe('desktop');
+
+    windowSize = 320;
+    await waitFor(() => {
+      triggerEventListener();
+    });
+    const newTestData = screen.getByTestId('test-data');
+    expect(getNodeText(newTestData)).toBe('mobile');
+  })
 });
